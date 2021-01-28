db4e1018bb83cee91fc04d9943a5fba1c5577798
Eonseo choi <kiop0511@gmail.com>
2021-01-28 13:47:57 +0900

    20210128

--- models/User.js
+++ models/User.js
@@ -6,14 +6,15 @@ var authToken = '11057e06b3c5ec710e1563664a2fb0df';
 
 var client = require('twilio')(accountSid, authToken);
 
+ran = String(getRandomInt());
+
 function getRandomInt() { //min ~ max 사이의 임의의 정수 반환
   var min = 11111
   var max = 99999
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-ran = String(getRandomInt());
-
+//sendMessage();
 (function sendMessage() { 
     client.messages
     .create({
@@ -23,9 +24,9 @@ ran = String(getRandomInt());
      })
    .then(message => console.log(message.sid));
   return ran;
-}());
+});
 
-//sendMessage()
+
 
 // schema
 var userSchema = mongoose.Schema({
@@ -89,7 +90,7 @@ userSchema.path('password').validate(function(v) {
   if(user.isNew){
     
     if(user.smsValidation !== ran){
-      user.invalidate('sms', '인증번호가 맞지 않습니다.  '+ ran)
+      user.invalidate('sms', '인증번호가 맞지 않습니다.'+ ran)
     }
       
     if(!user.passwordConfirmation){
