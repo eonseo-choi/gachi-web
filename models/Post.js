c8d789b305486e0dc925fa67fe27b9bbcba5fdd1
eonseo-choi <kiop0511@gmail.com>
2021-02-08 12:54:59 +0900

    backup before making qna board

--- models/Post.js
+++ models/Post.js
@@ -10,6 +10,27 @@ var postSchema = mongoose.Schema({
   updatedAt:{type:Date},
 });
 
+var postSchema = mongoose.Schema({
+  title:{type:String, required:[true,'Title is required!']},
+  body:{type:String, required:[true,'Body is required!']},
+  author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
+  attachment:{type:mongoose.Schema.Types.ObjectId, ref:'file'},
+  createdAt:{type:Date, default:Date.now},
+  updatedAt:{type:Date},
+});
+
+var postqnaSchema = mongoose.Schema({
+  title:{type:String, required:[true,'Title is required!']},
+  body:{type:String, required:[true,'Body is required!']},
+  author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
+  attachment:{type:mongoose.Schema.Types.ObjectId, ref:'file'},
+  createdAt:{type:Date, default:Date.now},
+  updatedAt:{type:Date},
+});
+
+
+
 // model & export
 var Post = mongoose.model('post', postSchema);
+var Post_qna = mongoose.model('postqna',postqnaSchema)
 module.exports = Post;
